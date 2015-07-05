fe0deb959cab11d79c865ca682b3156c002b0777
Zaro38 <github@niek.goudswaard.nl>
2015-07-05 21:06:40 +0200

    Update userscript.js
    stuff

--- userscript.js
+++ userscript.js
@@ -3,29 +3,35 @@
 //Made by PixelBreeze and Zaro38
 //
 function showCustomStuff() {
- $('.avatars .menu-points .dropdown dd').append('<div class="row" data-value="custom" id="custom"><span>Custom</span></div>');
+	$('.avatars .menu-points .dropdown dd').append('<div class="row" data-value="custom" id="custom"><span>Custom</span></div>');
 
- $('.avatars .menu-points .dropdown dd .row').on('click', function () {
-   if (this.id != 'custom') {
-    $('.avatars .menu-points .dropdown dd #custom').removeClass('selected');
-    $('.avatars .box .jspContainer .jspPane .grid .cell.custom').remove();
-   }
-   else {
-    $('.avatars .menu-points .dropdown dd .row').removeClass('selected');
-    $('.avatars .box .jspContainer .jspPane .grid').html('<div class="cell custom"><div class="top"><div class="avatar"><img/></div></div></div>');
-    $('.avatars .menu-points .dropdown dd #custom').addClass('selected');
-    $('.avatars .menu-points .dropdown dt span').html('Custom');
-   }
-  }
- )
+	$('.avatars .menu-points .dropdown dd .row').on('click', function () {
+			if (this.id != 'custom') {
+				$('.avatars .menu-points .dropdown dd #custom').removeClass('selected');
+				$('.avatars .box .jspContainer .jspPane .grid .cell.custom').remove();
+			}
+			else {
+				$('.avatars .menu-points .dropdown dd .row').removeClass('selected');
+				$('.avatars .box .jspContainer .jspPane .grid').html('<div class="cell custom"><div class="top"><div class="avatar"><img/></div></div></div>');
+				$('.avatars .menu-points .dropdown dd #custom').addClass('selected');
+				$('.avatars .menu-points .dropdown dt span').html('Custom');
+			}
+		}
+	)
 };
 
-$('#user-view #user-menu .item').on('click', function () {
- console.log('Menu Item Clicked !');
-  if ($(this).attr('data-value') == 'inventory' && $('.avatars .menu-points .dropdown dd #custom')[0] == undefined) {
-   console.log('Inventory Menu Clicked !')
-   showCustomStuff();
-  }
- }
-);
+if (typeof CasRunning == 'undefined') {
+	$('#user-view #user-menu .item').on('click', function () {
+			if ($(this).attr('data-value') == 'inventory' && $('.avatars .menu-points .dropdown dd #custom')[0] == undefined) {
+				showCustomStuff();
+			}
+		}
+	);
+}
+
+if ($('#user-view #user-menu .item.inventory').hasClass('selected') && typeof CasRunning == 'undefined') {
+	showCustomStuff();
+}
+
+var CasRunning = true;
 null;
